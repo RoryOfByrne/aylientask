0034a2ab91e8898ea64a7e7dce7eaeca0f4bb348
Rory Byrne <roryofbyrne@gmail.com>
2020-03-07 01:10:24 +0000

    Enforces authentication by default, and exempts the v1 batch route
    What does this commit do?
    =========================
    
    Adds DRF's IsAuthenticated permissions to the default permissions,
    enforcing it by default across all views. It also exempts the v1/batch
    view to maintain backwards compatibility. The CreateAccount view has
    already been exempt.
    
    Why are we doing this?
    ======================
    
    There's no reason for this API to be public, so we can at least control
    access in the future by establishing a foundation of strict permissions.

--- paint/settings/production.py
+++ paint/settings/production.py
@@ -83,7 +83,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication'],
-    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_THROTTLE_CLASSES': [] # TODO
 }
 
